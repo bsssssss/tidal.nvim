ea58aa74d09f2d1e1da37cd7593e878badef3879
Bss <mx.bss@proton.me>
2025-08-10 20:03:17 +0200

    (editor) move user commands here

--- lua/tidal/editor.lua
+++ lua/tidal/editor.lua
@@ -1,13 +1,25 @@
 local ghci = require("tidal.ghci")
+local postwin = require("tidal.postwindow")
 local format = require("tidal.format")
 local M = {}
+
+local function create_user_commands()
+	vim.api.nvim_buf_create_user_command(0, "TidalStart", ghci.start, {})
+	vim.api.nvim_buf_create_user_command(0, "TidalEval", M.eval, {})
+	vim.api.nvim_buf_create_user_command(0, "TidalHush", ghci.hush, {})
+	vim.api.nvim_buf_create_user_command(0, "TidalPost", postwin.toggle, {})
+	vim.api.nvim_buf_create_user_command(0, "TidalClear", postwin.clear, {})
+	vim.api.nvim_buf_create_user_command(0, "TidalStop", ghci.terminate, {})
+end
 
 local function create_autocmds()
 	local id = vim.api.nvim_create_augroup("tidal_editor", {})
 	vim.api.nvim_create_autocmd("FileType", {
 		group = id,
 		pattern = { "tidal", "tidal_post" },
-		callback = require("tidal.commands"),
+		callback = function()
+			create_user_commands()
+		end,
 	})
 	---@diagnostic disable-next-line: param-type-mismatch
 	vim.api.nvim_create_autocmd("VimLeavePre", {
